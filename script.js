9017e77b9b8734ef164ee99813ea1fc1d80a2998
ivolnoboi <irinavolnoboy@gmail.com>
2022-07-07 15:22:38 +0300

    The game has been created

--- script.js
+++ script.js
@@ -1,1 +1,81 @@
 'use strict';
+
+let secretNumber = generateRandom(1, 20);
+let score = 20;
+let highscore = 0;
+
+function generateRandom(min, max) {
+  return Math.trunc(Math.random() * max) + min;
+}
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+const displayNumber = function (number) {
+  document.querySelector('.number').textContent = number;
+};
+
+const displayScore = function (score) {
+  document.querySelector('.score').textContent = score;
+};
+
+const setViewToWin = function () {
+  document.querySelector('body').style.backgroundColor = '#60b347';
+  document.querySelector('.number').style.width = '30rem';
+};
+
+const setViewToLose = function () {
+  document.querySelector('body').style.backgroundColor = '#fc5e49';
+  document.querySelector('.number').style.width = '30rem';
+};
+
+const setViewToDefault = function () {
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  // When there is no input
+  if (!guess) {
+    displayMessage('⛔ No number!');
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 Correct Number!');
+    displayNumber(secretNumber);
+
+    setViewToWin();
+    if (score > highscore) {
+      highscore = score;
+    }
+    document.querySelector('.highscore').textContent = highscore;
+
+    // When guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+      score--;
+      displayScore(score);
+    } else {
+      displayMessage('💥 You lost the game!');
+      displayNumber(secretNumber);
+      setViewToLose();
+      displayScore(0);
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = generateRandom(1, 20);
+  score = 20;
+
+  displayMessage('Start guessing...');
+  displayNumber('?');
+  displayScore(score);
+  document.querySelector('.guess').value = '';
+
+  setViewToDefault();
+});
